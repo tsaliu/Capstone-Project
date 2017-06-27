4558c004e744485f58e1d270b672c4b4f07fc05a
Tsa Chun Liu <zachary821217@gmail.com>
2017-06-26 23:02:06 -0400

    Add files via upload

---
+++ Tracking/HSV tracking/build/CMakeFiles/objectTracking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/objectTracking.dir/objectTracking.cpp.o"
+  "objectTracking.pdb"
+  "objectTracking"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/objectTracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
